d0f2984b28207e1044b4970209ecc9a8f99f5d84
CJ Ogle <cj@pop-os.localdomain>
2018-08-14 22:33:16 -0400

    Fix typos.

--- docroot/welcome.php
+++ docroot/welcome.php
@@ -21,11 +21,11 @@
   <a class="file-link" href="/files/HHOA_Rules.pdf">HHOA Rules</a>
 </div>
 
-<p>Heatherstone has a volunteer board of directors that meet in the community clubhous at 6:30pm on the first Wednesday of each month. All are welcome to attend, and all are welcome to participate! Board elections are held once a year, but please feel free to help out anytime, there are always projects going on within the commminity that we could use your help on.</p>
+<p>Heatherstone has a volunteer board of directors that meet in the community clubhouse at 6:30pm on the second Wednesday of each month. All are welcome to attend, and all are welcome to participate! Board elections are held once a year, but please feel free to help out anytime, there are always projects going on within the commminity that we could use your help on.</p>
 
 <p>Heatherstone is a homeowners association and as such, there are monthly dues to be paid to the association. The current montly fee is $190 and it covers pool maintence, grounds maitenence, lawn mowing, snow removal and trash pickup. Please refer to the Declarations and Bylaws as well as the HHOA Rules for more information about the monthly dues.</p>
 
-<p> Please read through the Declarations and Bylaws as well as the HHOA Rulebook to have a better grasp on our community. Those documents will refer to some files that have not been listed here, but the can be found on the <a href="/all-files">HOA Files</a> page.
+<p>Please read through the Declarations and Bylaws as well as the HHOA Rulebook to have a better grasp on our community. Those documents will refer to some files that have not been listed here, but the can be found on the <a href="/all-files">HOA Files</a> page.
 
 <?php
   $content = ob_get_clean();
