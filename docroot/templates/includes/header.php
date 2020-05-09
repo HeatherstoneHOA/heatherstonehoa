cdbc348912196b5e1ed3370357009761fd10fed7
Curtis Ogle <curtis.jams.ogle@gmail.com>
2020-05-08 22:27:15 -0400

    Turn site into static html.

--- docroot/templates/includes/header.php
+++
@@ -1,6 +0,0 @@
-<div class="header">
-  <div class="header-container">
-    <a href="/" class="logo logo-abrv">HHOA</a>
-    <a href="/" class="logo logo-full">Heatherstone Homeowners' Association</a>
-  </div>
-</div>
